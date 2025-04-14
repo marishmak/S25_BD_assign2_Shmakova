6deaf605343f7b98383fea30893fdff576c19636
marishmak <shmak2804@mail.ru>
2025-04-14 22:45:48 +0300

    fixing + adding report

--- app/mapreduce/mapper2.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    try:
-        doc_id, title, text = line.strip().split('\t')
-        for term in text.split():
-            print(f"{term}\t{doc_id}\t1")
-    except Exception as e:
-        print(f"Error processing line: {line}", file=sys.stderr)
